e4ef2af0519f9a0e98a97b1521bebfe8e3ad98b1
Anthony Williams <anthony.williams.cs@gmail.com>
2018-10-24 23:28:27 -0600

    Fix project name

--- UWPFun/Properties/AssemblyInfo.cs
+++ UWPFun/Properties/AssemblyInfo.cs
@@ -2,26 +2,26 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CPlusPlusFun")]
+[assembly: AssemblyTitle("UWPFun")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CPlusPlusFun")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("UWPFun")]
+[assembly: AssemblyCopyright("Copyright © Anthony Williams 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
